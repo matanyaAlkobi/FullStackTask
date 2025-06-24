217487126240140b1b3ba19e081154e140f86441
matanya alkobi <matanor123456@gmail.com>
2025-06-24 16:02:48 +0300

    Data Massaging Practice -Completed

---
+++ week1/dataMassaging.js
@@ -0,0 +1,87 @@
+const data = [ 
+{ 
+name: "John", 
+birthday: "1-1-1995", 
+favoriteFoods: { 
+meats: ["hamburgers", "sausages"], 
+fish: ["salmon", "pike"], 
+}, 
+}, 
+{ 
+name: "Mark", 
+birthday: "10-5-1980", 
+favoriteFoods: { 
+meats: ["hamburgers", "steak", "lamb"], 
+fish: ["tuna", "salmon", "barracuda"], 
+}, 
+}, 
+{ 
+name: "Mary", 
+birthday: "1-10-1977", 
+favoriteFoods: { 
+meats: ["cow", "chicken"], 
+fish: ["pike"], 
+},
+}, 
+{ 
+name: "Thomas", 
+birthday: "1-10-1990", 
+favoriteFoods: { 
+meats: ["bird", "rooster"], 
+fish: ["salmon"], 
+}, 
+}, 
+{ 
+name: "Mary", 
+birthday: "1-10-1977", 
+favoriteFoods: { 
+meats: ["hamburgers", "lamb"], 
+fish: ["bass", "tuna"], 
+}, 
+}, 
+]; 
+
+
+function extractAllNames(arr){
+    let namesArray  = []
+    for (let item  of  arr){
+        namesArray.push(item.name)
+    }
+    return namesArray
+}
+// console.log(extractAllNames(data))
+
+function extractPeopleBornBefore1990(arr){
+    let dataOfPepoleBornBefore1990 = []
+    for (let obj of  arr){
+        const [day, month, year] = obj.birthday.split("-").map(Number);
+        const birthDate = new Date(year, month - 1, day);
+        const year1990 = new Date(1990, 0, 1);
+        if (birthDate < year1990){
+            dataOfPepoleBornBefore1990.push(obj);
+        }  
+    }
+    return dataOfPepoleBornBefore1990;
+}
+
+// console.log(extractPeopleBornBefore1990(data))
+
+
+function countFavoriteFoods(arr){
+    const foodData = data.flatMap(person => [...person.favoriteFoods.meats, ...person.favoriteFoods.fish])
+    const foodCounter = {}
+    for (let food of foodData){
+        if(foodCounter[food]){
+            foodCounter[food]++
+        }
+        else{
+            foodCounter[food] = 1
+        }
+    }
+    return foodCounter;
+}
+
+console.log(countFavoriteFoods(data));
+
+
+
